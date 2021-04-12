b48aeb2b4feeadf43eda4eafa7ac7d5b35aa77be
WhitestRabbit <stefanosmixalas@gmail.com>
2021-04-12 14:50:09 +0300

    -

--- pages/ergasies/index.js
+++ pages/ergasies/index.js
@@ -1,7 +1,16 @@
 import styles from '../../styles/Ergasies.module.css';
 import Head from 'next/head';
 
-const ergasies = () => {
+export const getStaticProps = async () => {
+    const res = await fetch("https://mockend.com/aueb-ergasies/Ergasies");
+    const data = await res.json();
+
+    return {
+        props: { ergasies: data }
+    }
+}
+
+const ergasies = ({ergasies}) => {
     return (
         <>
             <Head>
@@ -10,6 +19,14 @@ const ergasies = () => {
             </Head>
             <div>
                 <h1>Ergasies</h1>
+                {ergasies.map(ergasia => (
+                    <div key={ergasia.id}>
+                        <a>
+                            <h3>{ergasia.name}</h3>
+                        </a>
+                    </div>)
+                    )
+                }
             </div>
         </>
     );
